365a20b24234fc08712f70940700b3094dcacbed
CosZmo77 <syedsaadahmed77@gmail.com>
2025-12-16 17:16:34 +0530

    Footer chnages commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-/* ---------- Developer Icons ---------- */
-/* ---------- Developer Icons ---------- */
 const GitHubIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
